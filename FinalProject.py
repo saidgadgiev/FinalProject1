c774d3e4c995e6dedb700af77d442dc59c4e0770
Said Gadjiev <ackusha1984@ya.ru>
2023-01-25 01:12:02 +0300

    Создание блок схемы

--- FinalProject.py
+++ FinalProject.py
@@ -1,16 +1,18 @@
 import random
 #  Создание массива
-def Add_Array(num):
+def Add_Array():
+    num = random.randint(3,10)
     array=[]
+    print("*"*20)
     print(f"Будет создан массив из {num} элементов")
     print("*"*20)
     for i in range (num):
         text= input(f"Введите {i+1} -й элемент массива и нажмите Enter-> ")
         array.append(text)
     return array
-num_rand = random.randint(3,10)
 
-list = Add_Array(num_rand)
+
+list = Add_Array()
 print("*"*20)
 # Количество элементов для отбора
 number_rand = random.randint(0,3) # Случайный выбор цифр от 0 до 3 
